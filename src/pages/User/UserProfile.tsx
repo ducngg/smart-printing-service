3cffb29c184176b890c0115843f8992afe0e8b1b
luanvohcmut2911 <luan.vo2003@hcmut.edu.vn>
2023-11-28 23:17:00 +0700

    feat: add bar and charts

--- src/pages/User/UserProfile.tsx
+++ src/pages/User/UserProfile.tsx
@@ -1,7 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { ApexOptions } from 'apexcharts';
+import classNames from 'classnames';
 import React from 'react';
-import { Card, CardBody, CardTitle, Col, Container, Input, Label, Row } from 'reactstrap';
+import ReactApexChart from 'react-apexcharts';
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Col,
+  Container,
+  Input,
+  Label,
+  Nav,
+  NavItem,
+  Row,
+} from 'reactstrap';
 
 import Breadcrumb from 'Components/Common/Breadcrumb';
+import getChartColorsArray from 'Components/Common/ChartDynamicColor';
 import withRouter from 'Components/Common/withRouter';
 import useAppSelector from 'hooks/useAppSelector';
 import useTitle from 'hooks/useTitle';
@@ -9,12 +25,99 @@ import { RootState } from 'slices';
 
 import profileImg from '../../assets/images/profile-img.png';
 
+const options: ApexOptions = {
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: 'smooth',
+    width: 3,
+  },
+  colors: getChartColorsArray('["--bs-primary", "--bs-warning"]'),
+  xaxis: {
+    type: 'category',
+    categories: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'December',
+    ],
+  },
+  grid: {
+    borderColor: '#f1f1f1',
+  },
+  tooltip: {
+    x: {
+      format: 'dd/MM/yy HH:mm',
+    },
+  },
+};
+
+const chartOptions: ApexOptions = {
+  chart: {
+    stacked: !0,
+    zoom: {
+      enabled: !0,
+    },
+  },
+  plotOptions: {
+    bar: {
+      horizontal: !1,
+      columnWidth: '15%',
+      // endingShape: "rounded"
+    },
+  },
+  dataLabels: {
+    enabled: !1,
+  },
+  xaxis: {
+    categories: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ],
+    labels: {
+      show: true,
+    },
+  },
+  colors: getChartColorsArray('["--bs-primary", "--bs-warning"]'),
+  legend: {
+    position: 'bottom',
+  },
+  fill: {
+    opacity: 1,
+  },
+};
+
 const UserProfile = () => {
   useTitle('My Profile', {
     restoreOnUnmount: true,
   });
 
   const { user } = useAppSelector((state: RootState) => state.Login);
+
+  const [periodType, setPeriodType] = React.useState('weekly');
+
+  const onChangeChartPeriod = (pType: string) => {
+    setPeriodType(pType);
+  };
 
   return (
     <React.Fragment>
@@ -122,41 +225,136 @@ const UserProfile = () => {
           </Col>
         </Row>
 
-        {/* <Row>
-          <Col lg={6}>
+        <Row>
+          <Col lg={7}>
             <Card>
               <CardBody>
-                <CardTitle tag='h4' className='mb-4'>
-                  {' '}
-                  Spline Area{' '}
-                </CardTitle>
-                <Splinearea
-                  dataColors='["--bs-primary", "--bs-success"]'
-                  series={series}
-                  xAxis={xAxis}
-                />
-                <ReactApexChart
-                  options={areaChart}
-                  series={printRequestCount}
-                  type='area'
-                  height='350'
-                />
+                <div className='d-flex flex-wrap'>
+                  <h5 className='card-title me-2'> My print request count </h5>
+                  <div className='ms-auto'>
+                    <div className='toolbar d-flex flex-wrap gap-2 text-end'>
+                      <button type='button' className='btn btn-light btn-sm'>
+                        Today
+                      </button>
+                      <button type='button' className='btn btn-light btn-sm'>
+                        1M
+                      </button>
+                      <button type='button' className='btn btn-light btn-sm'>
+                        6M
+                      </button>
+                      <button type='button' className='btn btn-light btn-sm active'>
+                        1Y
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                <Row className='text-center'>
+                  <Col lg={4}>
+                    <div className='mt-4'>
+                      <p className='text-muted mb-1'>Today</p>
+                      <h5>1024</h5>
+                    </div>
+                  </Col>
+                  <Col lg={4}>
+                    <div className='mt-4'>
+                      <p className='text-muted mb-1'>This Month</p>
+                      <h5>12356</h5>
+                    </div>
+                  </Col>
+                  <Col lg={4}>
+                    <div className='mt-4'>
+                      <p className='text-muted mb-1'>This Year</p>
+                      <h5>102354</h5>
+                    </div>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <ReactApexChart
+                    options={options}
+                    series={[
+                      {
+                        name: 'Print request',
+                        data: [20, 23, 45, 33, 25, 43, 35, 44, 30, 40],
+                      },
+                    ]}
+                    type='area'
+                    height='350'
+                  />
+                </Row>
               </CardBody>
             </Card>
           </Col>
 
-          <Col lg={6}>
+          <Col lg={5}>
             <Card>
               <CardBody>
-                <CardTitle tag='h4' className='mb-4'>
-                  {' '}
-                  Column Chart{' '}
-                </CardTitle>
-                <Apaexlinecolumn dataColors='["--bs-danger","--bs-primary", "--bs-success"]' />
+                <div className='d-sm-flex flex-wrap'>
+                  <CardTitle tag='h4' className='mb-4'>
+                    Page count of each size
+                  </CardTitle>
+                  <div className='ms-auto'>
+                    <Nav pills>
+                      <NavItem>
+                        <a
+                          className={classNames({ active: periodType === 'weekly' }, 'nav-link')}
+                          onClick={() => {
+                            onChangeChartPeriod('weekly');
+                          }}
+                          id='one_month'
+                        >
+                          Week
+                        </a>
+                      </NavItem>
+                      <NavItem>
+                        <a
+                          className={classNames({ active: periodType === 'monthly' }, 'nav-link')}
+                          onClick={() => {
+                            onChangeChartPeriod('monthly');
+                          }}
+                          id='one_month'
+                        >
+                          {' '}
+                          Month{' '}
+                        </a>
+                      </NavItem>
+                      <NavItem>
+                        <a
+                          className={classNames({ active: periodType === 'yearly' }, 'nav-link')}
+                          onClick={() => {
+                            onChangeChartPeriod('yearly');
+                          }}
+                          id='one_month'
+                        >
+                          {' '}
+                          Year{' '}
+                        </a>
+                      </NavItem>
+                    </Nav>
+                  </div>
+                </div>
+                {/* TODO */}
+                <ReactApexChart
+                  options={chartOptions}
+                  series={[
+                    {
+                      name: 'A4',
+                      data: [20, 40, 20, 40, 20, 40, 20, 40, 20, 40],
+                    },
+                    {
+                      name: 'A3',
+                      data: [4, 3, 5, 12, 0, 12, 2, 5, 8, 1],
+                    },
+                  ]}
+                  type='bar'
+                  height='350'
+                  className='apex-charts'
+                />
               </CardBody>
             </Card>
           </Col>
-        </Row> */}
+        </Row>
       </div>
     </React.Fragment>
   );
